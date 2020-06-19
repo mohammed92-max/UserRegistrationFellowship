e2753d4a624b4ff0f8572909551b998ca03768af
mohammed92-max <shahrukhkhandb@gmail.com>
2020-06-19 18:19:36 +0530

    ADDED to accept atleast 1 numeric

--- src/main/java/com/bridgelabz/userRegistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userRegistration/UserRegistration.java
@@ -6,14 +6,14 @@ public class UserRegistration
 {
     public static boolean patternMatching( String pattern )
     {
-        return pattern.matches( "^(?=.*[A-Z])[a-zA-Z\\d]{8,}$" );
+        return pattern.matches( "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$" );
     }
 
     public static void main( String[] args )
     {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Enter the word with atleast 1 uppercase");
+        System.out.println("Enter the word with atleast 1 numeric number");
         String pattern = scan.nextLine();
         System.out.println(patternMatching(pattern));
     }
