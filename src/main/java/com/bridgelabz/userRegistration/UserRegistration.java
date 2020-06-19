bbd48bf16f0729ae4bd92b4abf400f08ca904608
mohammed92-max <shahrukhkhandb@gmail.com>
2020-06-19 18:08:51 +0530

    REFACTORED to validate last name

--- src/main/java/com/bridgelabz/userRegistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userRegistration/UserRegistration.java
@@ -4,18 +4,19 @@ import java.util.Scanner;
 
 public class UserRegistration
 {
-        public static boolean patternMatching( String pattern )
-        {
-            return pattern.matches( "^([A-Z]{1}[a-z]{2,})$" );
-        }
+    public static boolean patternMatching( String pattern )
+    {
+        return pattern.matches( "^([A-Z]{1}[a-z]{2,})$" );
+    }
 
-        public static void main( String[] args )
-        {
-            Scanner scan = new Scanner(System.in);
-            System.out.println("Enter the first name");
-            String pattern = scan.nextLine();
-            System.out.println(patternMatching(pattern));
-        }
+    public static void main( String[] args )
+    {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter the last name");
+        String pattern = scan.nextLine();
+        System.out.println(patternMatching(pattern));
+    }
 }
 
 
