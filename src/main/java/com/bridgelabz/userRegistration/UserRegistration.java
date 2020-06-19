4d8d0a58c1b0821c102279073198d3a04d1c681a
mohammed92-max <shahrukhkhandb@gmail.com>
2020-06-19 18:20:23 +0530

    ADDED to accept exactly 1 special character

--- src/main/java/com/bridgelabz/userRegistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userRegistration/UserRegistration.java
@@ -6,14 +6,14 @@ public class UserRegistration
 {
     public static boolean patternMatching( String pattern )
     {
-        return pattern.matches( "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$" );
+        return pattern.matches( "^(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$" );
     }
 
     public static void main( String[] args )
     {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Enter the word with atleast 1 numeric number");
+        System.out.println("Enter the word with which has exactly 1 special character");
         String pattern = scan.nextLine();
         System.out.println(patternMatching(pattern));
     }
