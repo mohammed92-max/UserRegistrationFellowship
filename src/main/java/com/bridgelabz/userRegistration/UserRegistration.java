1dceb5f09908067ea595635093c0f8f20b697b30
mohammed92-max <shahrukhkhandb@gmail.com>
2020-06-19 18:17:38 +0530

    ADDED to accept minimum 8 characters

--- src/main/java/com/bridgelabz/userRegistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userRegistration/UserRegistration.java
@@ -6,14 +6,14 @@ public class UserRegistration
 {
     public static boolean patternMatching( String pattern )
     {
-        return pattern.matches( "^([9]{1}[1]{1}([\\\\w ]+)[1-9]{1}[0-9]{9})$" );
+        return pattern.matches( "^([A-Z a-z]{8,})$" );
     }
 
     public static void main( String[] args )
     {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Enter mobile number");
+        System.out.println("Enter the word with minimum 8 characters");
         String pattern = scan.nextLine();
         System.out.println(patternMatching(pattern));
     }
