e46f383fd83fb29b5812bd6e4a1189af15bfe9c7
mohammed92-max <shahrukhkhandb@gmail.com>
2020-06-19 18:04:07 +0530

    REFACTORED to valid first name

--- src/main/java/com/bridgelabz/userRegistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userRegistration/UserRegistration.java
@@ -1,9 +1,27 @@
 package com.bridgelabz.userRegistration;
 
+import java.util.Scanner;
+
 public class UserRegistration
 {
-    public static void main(String[] args)
-    {
-        System.out.println("Welcome to User Registration program");
-    }
+        public static boolean patternMatching( String pattern )
+        {
+            return pattern.matches( "^([A-Z]{1}[a-z]{2,})$" );
+        }
+
+        public static void main( String[] args )
+        {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Enter the first name");
+            String pattern = scan.nextLine();
+            System.out.println(patternMatching(pattern));
+        }
 }
+
+
+
+
+
+
+
+
