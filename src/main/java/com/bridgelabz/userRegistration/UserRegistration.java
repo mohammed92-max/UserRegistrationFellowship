272f3e8cdac6c608bfa80edad5f706c49f8c0e24
mohammed92-max <shahrukhkhandb@gmail.com>
2020-06-19 15:55:34 +0530

    ADDED welcome file

---
+++ src/main/java/com/bridgelabz/userRegistration/UserRegistration.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.userRegistration;
+
+public class UserRegistration
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to User Registration program");
+    }
+}
